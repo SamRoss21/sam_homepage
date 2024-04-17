a9550d087cf810ab506ab6d8efc479bf77ab8eca
Sam <samhannahross@gmail.com>
2024-04-17 14:46:40 -0700

    Error fix

--- lib/sidebar.dart
+++ lib/sidebar.dart
@@ -17,7 +17,7 @@ class SideBar extends StatelessWidget {
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   fit: BoxFit.cover,
-                  image: AssetImage('Sam_Photo.jpg'),
+                  image: AssetImage("assets/Sam_Photo.jpeg"),
                 ),
                 borderRadius: BorderRadius.all(Radius.circular(20.0)),
               ),
@@ -27,16 +27,15 @@ class SideBar extends StatelessWidget {
               width: 200,
               margin: const EdgeInsets.only(top: 20, left: 250),
               child: Text.rich(TextSpan(
-                  style: const TextStyle(fontSize: 12, height: 3, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 51, 10, 111)),
                   children: [
                     const WidgetSpan(alignment: PlaceholderAlignment.middle, child: Icon(FontAwesomeIcons.locationPin, size: 15,)),
-                    const TextSpan(text: "  Seattle, WA\n"),
+                    const TextSpan(text: "  Seattle, WA\n", style: TextStyle(fontSize: 12, height: 3, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 51, 10, 111))),
                     const WidgetSpan(alignment: PlaceholderAlignment.middle, child: Icon(FontAwesomeIcons.envelope, size: 15,)),
-                    TextSpan(text: "  Email\n", recognizer: TapGestureRecognizer()..onTap = () => launchUrl(Uri.parse('mailto:samhross@cs.washington.edu'))),
+                    TextSpan(text: "  Email\n", recognizer: TapGestureRecognizer()..onTap = () => launchUrl(Uri.parse('mailto:samhross@cs.washington.edu')), style: TextStyle(fontSize: 12, height: 3, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 51, 10, 111))),
                     const WidgetSpan(alignment: PlaceholderAlignment.middle, child: Icon(FontAwesomeIcons.linkedin, size: 15,)),
-                    TextSpan(text: "  LinkedIn\n", recognizer: TapGestureRecognizer()..onTap = () => launchUrl(Uri.parse('https://www.linkedin.com/in/sam-ross-09936a190/'))),
+                    TextSpan(text: "  LinkedIn\n", recognizer: TapGestureRecognizer()..onTap = () => launchUrl(Uri.parse('https://www.linkedin.com/in/sam-ross-09936a190/')), style: TextStyle(fontSize: 12, height: 3, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 51, 10, 111))),
                     const WidgetSpan(alignment: PlaceholderAlignment.middle, child: Icon(FontAwesomeIcons.googleScholar, size: 15,)),
-                    TextSpan(text: "  Google Scholar\n", recognizer: TapGestureRecognizer()..onTap = () => launchUrl(Uri.parse('https://scholar.google.com/citations?user=aPo_CMMAAAAJ&hl=en'))),
+                    TextSpan(text: "  Google Scholar\n", recognizer: TapGestureRecognizer()..onTap = () => launchUrl(Uri.parse('https://scholar.google.com/citations?user=aPo_CMMAAAAJ&hl=en')), style: TextStyle(fontSize: 12, height: 3, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 51, 10, 111))),
 
                   ])),
             )
